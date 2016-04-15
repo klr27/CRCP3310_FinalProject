03da9b1cdcb1e43b71c9c6adb992194fa50d304b
David G. Smith <dgsmith7@hotmail.com>
2016-04-15 10:56:14 -0500

    Add stubs for d3 stuff.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 //javascript
 
-function load() {
+function drawViz() {
   alert("Hello World.");
+  //d3.select("example").append("svg").append("circle")....
 }
+
+window.addEventListener('load', drawViz, false );
