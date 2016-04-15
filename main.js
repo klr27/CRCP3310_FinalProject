bb3d842c7085c5e28c3c50c7a04a3deec0ead31d
Kali Ruppert <kruppert@smu.edu>
2016-04-15 11:48:35 -0500

    add hash tag

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var xArray = [100, 200, 300, 400, 500];
 var yArray = [40, 70, 20, 80, 40];
 
-var sampleSVG = d3.select("example")
+var sampleSVG = d3.select("#example")
         .append("svg")
         .attr("width", 100)
         .attr("height", 100);    
@@ -13,8 +13,8 @@ var sampleSVG = d3.select("example")
         .attr("r", 40)
         .attr("cx", 50)
         .attr("cy", 50)
-        .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-        .on("mouseout", function(){d3.select(this).style("fill", "white");});
+        //.on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+        //.on("mouseout", function(){d3.select(this).style("fill", "white");});
     
 function drawViz() {}
 
