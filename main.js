49d9f9f15146c976a9d3546c95a039d719df17d1
Kali Ruppert <kruppert@smu.edu>
2016-04-15 11:53:23 -0500

    adjust stlyesheet

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ var sampleSVG = d3.select("#example")
         .attr("height", 100);    
 
     sampleSVG.append("circle")
-        .style("stroke", "gray")
-        .style("fill", "white")
+        .style("stroke", "red")
+        .style("fill", "blue")
         .attr("r", 40)
         .attr("cx", 50)
-        .attr("cy", 50)
+        .attr("cy", 50);
         //.on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
         //.on("mouseout", function(){d3.select(this).style("fill", "white");});
     
