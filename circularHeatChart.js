f611a25726a5238f8a0b897694a2741382558c58
Kali Ruppert <kruppert@smu.edu>
2016-04-26 11:35:05 -0500

    add js file with circularHeatChart construct

---
+++ circularHeatChart.js
@@ -0,0 +1,13 @@
+//Create constructor for a heat chart object
+
+
+function circularHeatChart(innerRadius, ringHeight, name) {
+	this.radius = radius;
+	this.ringHeight = ringHeight;
+	this.name = name;
+	var numRings = 9;
+	var numArcs = 12;
+	var ringLabels = ["2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"];
+	var arcLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+}
